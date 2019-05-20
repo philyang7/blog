2bc1f3b4caee22eb640bbb8869cc3e3a9a1ad741
YvanYang <yvanyang@hotmail.com>
2019-05-20 17:10:36 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -15,8 +15,6 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m 'deploy'
-
-# 如果你想要部署到 https://<USERNAME>.github.io
 git push -f https://github.com/YvanYangi/YvanYangi.github.io.git master
 
 cd -
